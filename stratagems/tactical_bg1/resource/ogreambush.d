9ec09f085c0e78c88d4e1709f5a0bfd24b2394ac
DavidMWWallace <davidmwwallace@outlook.com>
2019-09-22 19:38:30 -0700

    Collated v32.6 fixes

--- stratagems/tactical_bg1/resource/ogreambush.d
+++ stratagems/tactical_bg1/resource/ogreambush.d
@@ -1,13 +1,8 @@
 BEGIN ~%tutu_scripto%GRELEAD~
-IF ~True()~ THEN BEGIN 0
+IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
 SAY @21502
 IF ~~ THEN DO
-	~SetGlobal("prep","LOCALS",1)
-	ApplySpellRES("%WIZARD_SPIRIT_ARMOR%",Myself)
-	ApplySpellRES("%WIZARD_MIRROR_IMAGE%",Myself)
-	ApplySpellRES("%WIZARD_PROTECTION_FROM_EVIL%",Myself)
-	ApplySpellRES("%WIZARD_MINOR_GLOBE_OF_INVULNERABILITY%",Myself)
-	ApplySpellRES("%WIZARD_PROTECTION_FROM_NORMAL_MISSILES%",Myself)~
+	~Enemy()~
 JOURNAL @21503
 EXIT END
 
