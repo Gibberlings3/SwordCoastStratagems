3f6089105709d938d53eb417dad217e898990e3a
Mike1072 <mike1072@users.noreply.github.com>
2020-02-22 22:47:44 -0800

    Release version 32.8

--- package_mod.bat
+++ package_mod.bat
@@ -6,7 +6,7 @@ call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
 set "mod_name=Sword Coast Stratagems"
-set mod_version=v31
+set mod_version=v32.8
 set mod_folder=stratagems
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
